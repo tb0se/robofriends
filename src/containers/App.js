f3cf7ef0672d841eac9f3dc306cc0d22d9a1c0b2
tb0se <rachiditbose@gmail.com>
2021-02-11 22:10:17 +0200

    react-hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,53 +6,45 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 
-class App extends Component{
-    constructor(){
-        super()
-        // Can change and affect the app
-        this.state = {
-            robots: [],
-            searchField:''
-        }
-    }
+function App() {
+ 
+    // Array Destructuring (STATES)
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    // Its part of react so no need for lamba func
-    componentDidMount(){
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => {this.setState({ robots:users })} );
-    }
+        .then(users => {setRobots(users)} );
+    },[])
 
     // Random name we create
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-        
-        //console.log(filteredRobots);
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
+
     }
 
-    render(){
+    const filteredRobots = robots.filter( robot =>{
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
 
-        const { robots, searchField } = this.state; // Destructuring
-        const filteredRobots = robots.filter( robot =>{
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        });
-        return !robots.length ? // 0 is false in JS
-        // Loading bar
-        <h1>Loading</h1> :
-        (
-            <div className='tc'>
+    return !robots.length ? // 0 is false in JS
+    // Loading bar
+    <h1>Loading</h1> :
+    (
+        <div className='tc'>
 
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots={filteredRobots}/>
-                    </ErrorBoundary>
-                </Scroll>
-                
-            </div>
-        );
-    }
+            <h1 className="f1">RoboFriends</h1>
+            <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                <ErrorBoundary>
+                    <CardList robots={filteredRobots}/>
+                </ErrorBoundary>
+            </Scroll>
+            
+        </div>
+    );
+    
 }
 
 export default App;
