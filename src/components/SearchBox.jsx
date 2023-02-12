00de8c3f5dd9e8d4967afd4d47f418c4b87d0469
tb0se <rachiditbose@gmail.com>
2023-02-12 21:46:55 +0200

    Migrate to vite using yarn

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const SearchBox = ({searchField, searchChange}) =>{
+const SearchBox = ({ searchField, searchChange }) => {
     return (
         <div className="pa2">
-            <input 
-                className='pa3 ba b--green bg-lightest-blue' 
+            <input
+                className='pa3 ba b--green bg-lightest-blue'
                 type='search'
                 placeholder='Search robots'
                 onChange={searchChange}
             />
         </div>
-        
+
     );
 }
 
